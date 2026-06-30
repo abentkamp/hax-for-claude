a5c4a0d8763a969e57438ef58abc2a48e6415fc6
Alexander Bentkamp <alexander@cryspen.com>
2026-06-30 11:58:04 +0200

    chore: remove old core-models

--- hax-lib/core-models/src/core/default.rs
+++
@@ -1,7 +0,0 @@
-/// See [`std::default::Default`]
-#[hax_lib::attributes]
-pub trait Default {
-    /// See [`std::default::Default::default`]
-    #[hax_lib::requires(true)]
-    fn default() -> Self;
-}
